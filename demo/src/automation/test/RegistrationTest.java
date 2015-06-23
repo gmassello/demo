f4030aaa99e798e128af1d58a50f671f80c4e22d
German Massello <gmassello@hotmail.com>
2015-06-22 22:00:48 -0300

    .

---
+++ demo/src/automation/test/RegistrationTest.java
@@ -0,0 +1,17 @@
+package automation.test;
+
+import org.testng.annotations.Test;
+
+import automation.actions.RegistrationActions;
+import automation.helper.StaticProvider;
+import automation.test.BaseTest;
+
+public class RegistrationTest extends BaseTest {
+	
+	@Test(dataProvider = "demoInformation", dataProviderClass = StaticProvider.class)
+	public void fieldsValidation(String user, String password) {
+		RegistrationActions registration = new RegistrationActions();
+		registration.fieldsValidation(user, password, driver);
+	}
+
+}
